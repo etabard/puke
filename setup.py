d2624c0518124057bf9a30dfc85729a3cf8bed58
Manu <e.tabard@gmail.com>
2012-10-31 23:44:08 +0100

    troubles with python-requests 0.14.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['pyscss', 'closure_linter', 'colorama', 'pyyaml', 'paramiko', 'requests'],
+    install_requires = ['pyscss', 'closure_linter', 'colorama', 'pyyaml', 'paramiko', 'requests==0.13.1'],
     dependency_links = ['http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz'],
   
     # metadata for upload to PyPI
