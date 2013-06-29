9cf483180ce259c36d2413ce837afcb6e4c35231
Low Kian Seong <kianseong@gmail.com>
2013-06-29 21:02:44 +0800

    taking out pep8 violations

--- puke/Require.py
+++ puke/Require.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 """REquire parser
 
@@ -8,13 +8,13 @@ This class simply provide an accessor on a dict
 
 
 '''generic:
-	titi: "dtc"
+        titi: "dtc"
 
 dev:
-	toto: "${BOURSE_PATH}|/toto/coucou"
-	titi: "${BOURSE_PATH}|/toto/coucou"
-	coin:
-		toto: 'cul'
+        toto: "${BOURSE_PATH}|/toto/coucou"
+        titi: "${BOURSE_PATH}|/toto/coucou"
+        coin:
+                toto: 'cul'
 
 
 
@@ -45,8 +45,10 @@ coin = [toto: vf,;:]
 
 CONFIG['toto']'''
 
-import os,re
-import yaml,json
+import os
+import re
+import yaml
+import json
 from puke.Env import *
 from puke.Console import *
 from puke.Yak import *
@@ -61,134 +63,131 @@ yaml.add_constructor(u'tag:yaml.org,2002:str', custom_str_constructor)
 
 class Load(object):
 
-	def __init__(self, filename):
-		self.content = None
+        def __init__(self, filename):
+                self.content = None
 
-		try:
-			stream = None
-			stream = file(filename, 'r')
+                try:
+                        stream = None
+                        stream = file(filename, 'r')
 
-			payload = stream.read()
-			stream.close()
+                        payload = stream.read()
+                        stream.close()
 
-			size = payload.strip()
+                        size = payload.strip()
 
-			ext = os.path.splitext(stream.name)[1]
+                        ext = os.path.splitext(stream.name)[1]
 
-			if size == 0:
-				self.content = { }
-				return
-			
-			if ext in ['.json', '.js']:
-				self.content = json.loads(payload)
-			elif ext in ['.yaml', '.yml']:
-				self.content = yaml.load(payload)
-						
-		except Exception as error :
-			raise RequireError("Require load error : %s" % error)
-			self.content = None
+                        if size == 0:
+                                self.content = {}
+                                return
 
-	def __str__(self):
-		return "%s" % self.content
+                        if ext in ['.json', '.js']:
+                                self.content = json.loads(payload)
+                        elif ext in ['.yaml', '.yml']:
+                                self.content = yaml.load(payload)
 
-	def __repr__(self):
-		return "%s" % self.content
+                except Exception as error:
+                        raise RequireError("Require load error : %s" % error)
+                        self.content = None
+
+        def __str__(self):
+                return "%s" % self.content
+
+        def __repr__(self):
+                return "%s" % self.content
 
 
 class Require(object):
 
-	__sharedState = {}
-	__globalPattern = re.compile('\$\{([^}]+)\}([|])?(.*)')
+        __sharedState = {}
+        __globalPattern = re.compile('\$\{([^}]+)\}([|])?(.*)')
 
+        def __init__(self, filename):
+                self.__dict__ = self.__sharedState
+                if not self.__sharedState:
+                        self.__files = [filename]
+                        self.__cfg = Load(filename).content
+                        self.__makeenvs(self.__cfg)
 
-	def __init__(self, filename):
-		self.__dict__ = self.__sharedState
-		if not self.__sharedState:
-			self.__files = [filename]
-			self.__cfg = Load(filename).content
-			self.__makeenvs(self.__cfg)
-			
+        def merge(self, filename):
+                self.__files.append(filename)
+                self.__cfg = deepmerge(self.__cfg, Load(filename).content)
+                self.__makeenvs(self.__cfg)
 
-	def merge(self, filename):
-		self.__files.append(filename)
-		self.__cfg = deepmerge(self.__cfg, Load(filename).content)
-		self.__makeenvs(self.__cfg)
+        def yak(self, selector):
+                if not self.get(selector):
+                        return False
 
-	def yak(self, selector):
-		if not self.get(selector):
-			return False
+                for (node, value) in self.get(selector).items():
+                        existing = Yak.get(node)
+                        if existing is not None and not isinstance(existing,
+                                                                   str):
+                                value = deepmerge(Yak.get(node), value)
 
-		for (node, value) in self.get(selector).items():
-			existing = Yak.get(node)
-			if existing != None and not isinstance(existing, str):
-				value = deepmerge(Yak.get(node), value)
-							
-			Yak.set(node,value)
-		
-		
+                        Yak.set(node, value)
 
-	def get(self, key):
-		if self.__cfg.has_key(key):
-			return self.__cfg[key]
+        def get(self, key):
+                if key in self.__cfg:
+                        return self.__cfg[key]
 
-	def set(self, key, value):
-		self.__cfg[key] = value
+        def set(self, key, value):
+                self.__cfg[key] = value
 
-	def __getitem__(self, key):
-		return self.get(key)
+        def __getitem__(self, key):
+                return self.get(key)
 
-	def __setitem__(self, key, value):
-		self.set(key, value)
+        def __setitem__(self, key, value):
+                self.set(key, value)
 
-	def __contains__(self, key):
-		return (key in self.__cfg)
+        def __contains__(self, key):
+                return (key in self.__cfg)
 
-	def __repr__(self):
-		return 'Config: %s' % self.__cfg
-	
-	def __makeenvs(self, data ):
-		if data == None:
-			return False
+        def __repr__(self):
+                return 'Config: %s' % self.__cfg
 
-		if isinstance(data, list):
-			dataIter = enumerate(data)
-		elif isinstance(data, dict):
-			dataIter = data.items()
-		else:
-			return
+        def __makeenvs(self, data):
+                if data is None:
+                        return False
 
-		
-		for (node, value) in dataIter:
-			if not isinstance(value, (str, int)):
-				self.__makeenvs(value)
-			elif isinstance(value, str) and value.startswith('${'):
-				m = self.__globalPattern.match(value)
-				if not m:
-					continue
-				
-				(name, isDefault , extValue) = m.groups()
-				if isDefault:
-					default = extValue
-				else:
-					default = ''
+                if isinstance(data, list):
+                        dataIter = enumerate(data)
+                elif isinstance(data, dict):
+                        dataIter = data.items()
+                else:
+                        return
 
-				value = Env.get(name, default)
+                for (node, value) in dataIter:
+                        if not isinstance(value, (str, int)):
+                                self.__makeenvs(value)
+                        elif isinstance(value, str) and value.startswith('${'):
+                                m = self.__globalPattern.match(value)
+                                if not m:
+                                        continue
 
-				if not isDefault:
-					value += extValue
+                                (name, isDefault, extValue) = m.groups()
+                                if isDefault:
+                                        default = extValue
+                                else:
+                                        default = ''
 
-				data[node] = value
-				
+                                value = Env.get(name, default)
 
-	def reload(self):
-		files = self.__files
-		self.__files = []
-		self.__cfg = {}
-		for filename in files:
-			self.include(filename)
+                                if not isDefault:
+                                        value += extValue
+
+                                data[node] = value
+
+        def reload(self):
+                files = self.__files
+                self.__files = []
+                self.__cfg = {}
+                for filename in files:
+                        self.include(filename)
+
 
 class RequireError(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+        def __init__(self, value):
+                self.value = value
+
+        def __str__(self):
+                return repr(self.value)
